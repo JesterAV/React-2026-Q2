0873bba4cd726127349ac9949c9d4230191594db
Aleksei Bulash <winchesrers@gmail.com>
2026-05-05 00:41:59 +0300

    chore: update dependecie and vite config

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/class-components/',
   plugins: [react()],
   build: {
     outDir: 'dist',
