a913b32467acc36e5ce4bcb2c5dcb4e5ee46acd1
Aleksei Bulash <winchesrers@gmail.com>
2026-05-05 00:28:31 +0300

    chore: update vite config

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+
+  server: {
+    port: 7777,
+    strictPort: true,
+    open: true,
+  },
+});
