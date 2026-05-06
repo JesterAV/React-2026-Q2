daa5a4049d76b6099a64deb1c0658029f5d72c54
Aleksei <aleksei@MacBook-Pro-Aleksei.local>
2026-05-06 12:05:30 +0300

    style: add variables and media requests

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import './styles/globals.css'
+import './styles/variables.css'
 import MainPage from './pages/main'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
