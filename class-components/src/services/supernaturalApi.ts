c3336565cbcb80fb0be6b534115182494377c29b
Aleksei Bulash <winchesrers@gmail.com>
2026-05-18 23:59:58 +0300

    feat: add get character by id method

--- class-components/src/services/supernaturalApi.ts
+++ class-components/src/services/supernaturalApi.ts
@@ -1,19 +1,25 @@
 import { apiConfig } from "../config/api"
 
 export const supernaturalApi = {
-  fetchAllCharacters: async () => {
-    const response = await fetch(`${apiConfig.baseUrl}${apiConfig.endpoints.characters}?page=1&size=30`);
+  fetchAllCharacters: async (page: number = 1) => {
+    const response = await fetch(`${apiConfig.baseUrl}${apiConfig.endpoints.characters}?page=${page}&size=${apiConfig.defaultPageSize}`);
     
     if (!response.ok) throw new Error ('Failed to fetch all users');
     
     return response.json();
   },
 
-  searchCharacter: async (query: string) => { 
-    const response = await fetch(`${apiConfig.baseUrl}${apiConfig.endpoints.characters}?name=${query.trim()}`);
+  searchCharacter: async (query: string, page: number = 1) => { 
+    const response = await fetch(`${apiConfig.baseUrl}${apiConfig.endpoints.characters}?name=${query.trim()}&page=${page}&size=${apiConfig.defaultPageSize}`);
 
     if (!response.ok) throw new Error ('Failed to search');
 
+    return response.json();
+  },
+
+  getCharacterById: async (id: string) => {
+    const response = await fetch(`${apiConfig.baseUrl}${apiConfig.endpoints.characters}/${id}`);
+    
     return response.json();
   }
 }
