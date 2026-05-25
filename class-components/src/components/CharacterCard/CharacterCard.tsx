29029dedeabc86bdbd2813c233f3f2d4a36eec3d
Aleksei Bulash <winchesrers@gmail.com>
2026-05-25 21:59:36 +0300

    feat: add checkbox

--- class-components/src/components/CharacterCard/CharacterCard.tsx
+++ class-components/src/components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,7 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSelectCard } from '../../store/slices/selectCards';
 import './CharacterCard.scss';
+import type { RootState } from '../../types/state';
 
 interface CharacterCardProps {
   name: string;
@@ -11,14 +14,26 @@ interface CharacterCardProps {
 function CharacterCard(props: CharacterCardProps) {
   const {name, img, actor, id, setId} = props;
 
+  const dispatch = useDispatch();
+  
+  const selectedCards = useSelector((state: RootState) => state.selectedCards.selectCards);
+  const isSelected = selectedCards.includes(id);
+
   const handleClick = () => {
     setId(id);
+  }
+
+  const handleCheckboxChange = (e: React.ChangeEvent) => {
+    e.stopPropagation();
+    dispatch(toggleSelectCard(id));
   }
 
   return (
     <div className="character-card" onClick={() => handleClick()}>
 
       <img className='character-card__img' src={img} alt={name} />
+
+      <input type='checkbox' onChange={handleCheckboxChange} checked={isSelected} onClick={(e) => e.stopPropagation()} />
 
       <div className='character-card__separator'></div>
 
