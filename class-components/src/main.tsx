dfd4e525592a32be5a7dd3f2b33de3c3aee2ac00
Aleksei Bulash <winchesrers@gmail.com>
2026-05-25 22:18:04 +0300

    feat: wrap application to theme context

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,15 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import './styles/globals.css'
 import './styles/variables.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 import AppRouter from './router/router'
+import { Provider } from 'react-redux';
+import { store } from './store/store'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <AppRouter />
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <AppRouter />
+        </ErrorBoundary>
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
