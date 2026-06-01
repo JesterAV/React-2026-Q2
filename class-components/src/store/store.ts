80a43e79812b5a50100bce6657678d62a2af0981
Aleksei Bulash <winchesrers@gmail.com>
2026-06-02 02:52:12 +0300

    feat: init api

--- class-components/src/store/store.ts
+++ class-components/src/store/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedCardsReducer from './slices/selectCards';
+import { supernaturalApi } from './api/supernaturalApi';
 
 export const store = configureStore({
   reducer: {
-    selectedCards: selectedCardsReducer
-  }
+    selectedCards: selectedCardsReducer,
+    [supernaturalApi.reducerPath]: supernaturalApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(supernaturalApi.middleware)
+  },
 })
