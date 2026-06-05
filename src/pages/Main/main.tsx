e1023007f94f3c9cb2ede0ba4546323602bdbc9a
Aleksei Bulash <winchesrers@gmail.com>
2026-06-05 21:05:13 +0300

    feat: create main page

---
+++ src/pages/Main/main.tsx
@@ -0,0 +1,25 @@
+import './main.scss';
+
+import Button from '../../components/Button/Button';
+import ModalForm from '../../components/ModalForm/ModalForm';
+import { useModal } from '../../hooks/useModal';
+
+export default function MainPage() {
+  const { openModal, closeModal, isOpen } = useModal();
+
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) closeModal();
+  };
+
+  return (
+    <div className="main-page" onClick={closeModal}>
+      {isOpen && (
+        <div className="modal-overlay" onClick={handleOverlayClick}>
+          <ModalForm closeHandler={closeModal} />
+        </div>
+      )}
+
+      <Button type="button" text="Open Modal" onClick={openModal} />
+    </div>
+  );
+}
