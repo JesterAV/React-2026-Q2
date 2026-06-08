2a0d69b73700a43feaebf7b6b4b365905200b301
Aleksei Bulash <winchesrers@gmail.com>
2026-06-09 00:26:05 +0300

    feat: add selected input

--- src/components/ModalForm/components/FormField/SelectFormFiled.tsx
+++ src/components/ModalForm/components/FormField/SelectFormFiled.tsx
@@ -1,44 +1,50 @@
-import { useId, useState } from 'react';
+import { forwardRef, useId } from 'react';
 
-interface SelectFormFieldProps {
+interface SelectFormFieldProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   labelText: string;
   placeholder: string;
   options: string[];
+  value?: string | number;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-export default function SelectFormField(props: SelectFormFieldProps) {
-  const { labelText, options, placeholder } = props;
+const SelectFormField = forwardRef<HTMLSelectElement, SelectFormFieldProps>(
+  (props, ref) => {
+    const { labelText, options, placeholder, value, onChange, ...rest } = props;
 
-  const [selectValue, setSelectValue] = useState<string>('');
-  const fieldId = useId();
+    const fieldId = useId();
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectValue(e.target.value);
-  };
+    const isControlled = onChange !== undefined;
 
-  return (
-    <div className="form-field">
-      <label htmlFor={fieldId}>{labelText}</label>
-      <select
-        className="form-field__select"
-        id={fieldId}
-        value={selectValue}
-        onChange={handleChange}
-        required
-      >
-        <option className="form-field__select_option" value="" disabled>
-          {placeholder}
-        </option>
-        {options.map((value) => (
-          <option
-            className="form-field__select_option"
-            key={value}
-            value={value}
-          >
-            {value}
+    return (
+      <div className="form-field">
+        <label htmlFor={fieldId}>{labelText}</label>
+        <select
+          ref={ref}
+          className="form-field__select"
+          id={fieldId}
+          {...(isControlled ? { value: value ?? '' } : { defaultValue: '' })}
+          required
+          {...rest}
+          onChange={onChange}
+        >
+          <option className="form-field__select_option" value="" disabled>
+            {placeholder}
           </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+          {options.map((value) => (
+            <option
+              className="form-field__select_option"
+              key={value}
+              value={value}
+            >
+              {value}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+);
+
+SelectFormField.displayName = 'SelectFormField';
+export default SelectFormField;
