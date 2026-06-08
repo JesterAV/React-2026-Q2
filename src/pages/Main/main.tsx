3f80ef1f7926bef21361eea6f5f535e11595c35f
Aleksei Bulash <winchesrers@gmail.com>
2026-06-09 00:07:08 +0300

    feat: create main page

--- src/pages/Main/main.tsx
+++ src/pages/Main/main.tsx
@@ -3,9 +3,18 @@ import './main.scss';
 import Button from '../../components/Button/Button';
 import ModalForm from '../../components/ModalForm/ModalForm';
 import { useModal } from '../../hooks/useModal';
+import UncontrolledForm from '../../components/ModalForm/components/Form/UncontrolledForm';
+import ReactHookForm from '../../components/ModalForm/components/Form/ReactHookForm';
+import AnswersList from '../../components/AnswersList/AnswersList';
 
 export default function MainPage() {
-  const { openModal, closeModal, isOpen } = useModal();
+  const {
+    openModal,
+    closeModal,
+    isOpen,
+    openReactHookForm,
+    setOpenReactHookForm,
+  } = useModal();
 
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) closeModal();
@@ -15,11 +24,29 @@ export default function MainPage() {
     <div className="main-page">
       {isOpen && (
         <div className="modal-overlay" onClick={handleOverlayClick}>
-          <ModalForm closeHandler={closeModal} />
+          {openReactHookForm ? (
+            <ModalForm closeHandler={closeModal}>
+              <ReactHookForm />
+            </ModalForm>
+          ) : (
+            <ModalForm closeHandler={closeModal}>
+              <UncontrolledForm />
+            </ModalForm>
+          )}
         </div>
       )}
 
-      <Button type="button" text="Open Modal" onClick={openModal} />
+      <Button type="button" text="Open uncontrolled form" onClick={openModal} />
+      <Button
+        type="button"
+        text="Open React Hook Form"
+        onClick={() => {
+          openModal();
+          setOpenReactHookForm(true);
+        }}
+      />
+
+      <AnswersList />
     </div>
   );
 }
