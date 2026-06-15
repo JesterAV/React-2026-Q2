a43bb346c4eb957c1e70db0e409a1f184705302c
Aleksei Bulash <winchesrers@gmail.com>
2026-06-15 19:24:19 +0300

    feat: create app layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>class-components</title>
+      </head>
+      <body>
+        <div id="root">{children}</div>
+        <script type="module" src="/src/main.tsx"></script>
+      </body>
+    </html>
+  )
+}
