f548ee0cd1b3749074e2c4606c98cdaa166ac29d
Aleksei Bulash <winchesrers@gmail.com>
2026-06-17 18:41:01 +0300

    feat: replace image tag

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,15 +1,16 @@
 import './about.scss';
-import logo from '../../assets/supernatural_logo.png';
+import logo from '../../../public/supernatural_logo.png';
 import Button from '../../components/Button/Button';
 import { useNavigate } from 'react-router';
 import { appRoutes } from '../../router/routes';
+import Image from 'next/image';
 
 function AboutPage() {
   const navigate = useNavigate();
 
   return (
     <div className='about-page'>
-      <img src={logo} alt='supernatural logo' className='about-page__image' />
+      <Image src={logo} alt='supernatural logo' width={200} height={200} />
       <p className='about-page__text'>This application offers a search engine in which you can find characters from the Supernatural series.</p>
       <p className='about-page__text'>Author: <a href='https://github.com/JesterAV' target='_blank' className='about-page__link'>JesterAV</a></p>
       <Button text='Go to main page' type='button' onClick={() => navigate(appRoutes.main)} />
