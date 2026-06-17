10a46316fdbbc3c8607ca57ae9628c156e4159a6
Aleksei Bulash <winchesrers@gmail.com>
2026-06-17 18:38:13 +0300

    feat: replace image tag

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,15 +1,16 @@
 import './404.scss';
-import logo from '../../assets/supernatural_logo.png';
+import logo from '../../../public/supernatural_logo.png';
 import Button from '../../components/Button/Button';
 import { useNavigate } from 'react-router';
 import { appRoutes } from '../../router/routes';
+import Image from 'next/image';
 
 function NotFoundPage() {
   const navigate = useNavigate();
 
   return (
     <div className='not-found-page'>
-      <img src={logo} alt='supernatural logo' className='not-found-page__image' />
+      <Image src={logo} alt='supernatural logo' width={170} height={170} />
 
       <h1 className='not-found-page__title'>404</h1>
       <p className='not-found-page'>{'Ooops! Page not found :('}</p>
