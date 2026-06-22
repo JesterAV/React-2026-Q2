21c294d13cb07a3ec4a3d838867c5b5301fbd323
Aleksei Bulash <winchesrers@gmail.com>
2026-06-22 21:14:23 +0300

    feat: update hook

--- src/hooks/useCharacterDetail.ts
+++ src/hooks/useCharacterDetail.ts
@@ -1,22 +1,23 @@
-import { useSearchParams } from "react-router";
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from '../i18n/navigation';
 
 export function useCharacterDetail() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   const openDetails = (id: string): void => {
-    const params = new URLSearchParams(searchParams);
-    params.set('details', id);
-    setSearchParams(params);
-  }
+    const params = new URLSearchParams(searchParams?.toString() ?? '');
+    params.set('detailId', id);
+    router.push(`?${params.toString()}`);
+  };
 
   const closeDetails = (): void => {
-    const params = new URLSearchParams(searchParams);
-    params.delete('details');
-    setSearchParams(params);
-  }
+    const params = new URLSearchParams(searchParams?.toString() ?? '');
+    params.delete('detailId');
+    router.push(`?${params.toString()}`);
+  };
 
-  return {
-    openDetails,
-    closeDetails
-  }
+  return { openDetails, closeDetails };
 }
