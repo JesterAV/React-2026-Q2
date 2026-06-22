efa32f0952e12f591bc2bdb05ec1c0f45778e4e1
Aleksei Bulash <winchesrers@gmail.com>
2026-06-22 21:00:39 +0300

    feat: add translations

--- src/components/ResultContainer/ResultContainerError.tsx
+++ src/components/ResultContainer/ResultContainerError.tsx
@@ -1,8 +1,14 @@
+'use client';
+
+import { useTranslations } from "next-intl";
+
 function ResultContainerError() {
+  const t = useTranslations('resultContainerError');
+
   return (
     <div className="result-container__error">
       <h2 className="result-container__message">
-        No characters found, please try another request.
+        {t('resultContainerErrorText')}
       </h2>
     </div>
   )
