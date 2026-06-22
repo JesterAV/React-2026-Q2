cff65494bb124307718a6bdc6430f633640255c0
Aleksei Bulash <winchesrers@gmail.com>
2026-06-22 21:16:43 +0300

    feat: init select charcter reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import selectedCardsReducer from './slices/selectCards';
 import { supernaturalApi } from './api/supernaturalApi';
+import selectedCharactsReducer from './slices/characters';
 
 export const store = configureStore({
   reducer: {
     selectedCards: selectedCardsReducer,
-    [supernaturalApi.reducerPath]: supernaturalApi.reducer
+    [supernaturalApi.reducerPath]: supernaturalApi.reducer,
+    selectedCharacters: selectedCharactsReducer
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(supernaturalApi.middleware)
